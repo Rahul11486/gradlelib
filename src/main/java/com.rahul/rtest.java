202d29a5a29da1b87542611f8abb627224a13216
Rahul Srivastava <rahul@test.com>
2016-09-27 19:54:38 +0100

    initial

---
+++ src/main/java/com.rahul/rtest.java
@@ -0,0 +1,16 @@
+package com.rahul;
+import java.util.Scanner;
+/**
+ * Created by ng5u02 on 27/09/2016.
+ */
+public class rtest {
+    public static void main(String[] args) {
+        Scanner ab = new Scanner(System.in);
+        int a, b,c;
+        a = 10;
+        b = 4;
+        c = a+b;
+        System.out.println("Sum :"+c);
+
+    }
+}
