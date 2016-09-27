0a637dc1c46ad48cc5bbc6400cd8471bbf4f9f35
Rahul Srivastava <rahul@test.com>
2016-09-27 19:23:42 +0100

    made changes

--- src/main/java/com.rahul/X.java
+++ src/main/java/com.rahul/X.java
@@ -2,6 +2,6 @@ package com.rahul;
 
 public class X {
     public static void main(String[] args) {
-        System.out.println("test gradle");
+        System.out.println("test gradle 2");
     }
 }
