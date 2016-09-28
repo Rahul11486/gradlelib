65e152e8ff957c815ada0266437c5cca53e89cd9
Rahul Srivastava <rahul@test.com>
2016-09-28 13:24:56 +0100

    Testing

---
+++ src/main/java/com/rah_mongo/Rah_Test.java
@@ -0,0 +1,10 @@
+package com.rah_mongo;
+
+/**
+ * Created by ng5u02 on 28/09/2016.
+ */
+public class Rah_Test {
+    public static void main(String[] args) {
+        System.out.println("Testing");
+    }
+}
